4f4db7b9decfdd7b2cb08798d6ad47f33d776d1f
AXeL-dev <anass_denna@hotmail.fr>
2019-01-28 03:27:57 +0100

    beta release: v2.0.0

--- lib/shared.py
+++ lib/shared.py
@@ -5,7 +5,7 @@ __program_name__  = 'Dindo Bot'
 __program_desc__  = 'Farming bot for Dofus game'
 __website__       = 'https://github.com/AXeL-dev'
 __website_label__ = 'AXeL-dev'
-__version__       = '1.1.0'
+__version__       = '2.0.0-beta'
 __authors__       = ['AXeL']
 
 class LogType:
