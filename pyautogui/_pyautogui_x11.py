883769ce798170e28baf02cf01c19a624ac7cbe5
AXeL-dev <contact.axel.dev@gmail.com>
2020-04-13 12:26:58 +0100

    Update pyautogui to the latest version

--- pyautogui/_pyautogui_x11.py
+++ pyautogui/_pyautogui_x11.py
@@ -3,15 +3,15 @@
 import pyautogui
 import sys
 import os
+from pyautogui import LEFT, MIDDLE, RIGHT
 
 from Xlib.display import Display
 from Xlib import X
 from Xlib.ext.xtest import fake_input
 import Xlib.XK
 
-BUTTON_NAME_MAPPING = {'left': 1, 'middle': 2, 'right': 3, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}
+BUTTON_NAME_MAPPING = {LEFT: 1, MIDDLE: 2, RIGHT: 3, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}
 
-MOUSE_EVENT = {1: 'left', 2: 'middle', 3: 'right'}
 
 if sys.platform in ('java', 'darwin', 'win32'):
     raise Exception('The pyautogui_x11 module should only be loaded on a Unix system that supports X11.')
@@ -96,41 +96,6 @@ def _mouseUp(x, y, button):
     fake_input(_display, X.ButtonRelease, button)
     _display.sync()
 
-def _getMouseEvent():
-    screen = _display.screen()
-    window = screen.root
-    window.grab_pointer(1, X.PointerMotionMask|X.ButtonReleaseMask|X.ButtonPressMask, X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE, X.CurrentTime)
-    e = _display.next_event()
-    _display.ungrab_pointer(X.CurrentTime)
-    
-    if e.type == X.ButtonPress:
-        e = MOUSE_EVENT[e.detail] + '_down'
-        
-    elif e.type == X.ButtonRelease:
-        e = MOUSE_EVENT[e.detail] + '_up'
-        
-    else:
-        e = 'moving'
-          
-    return e
-
-def _waitForMouseEvent(e):
-    
-    assert e in ['left_down', 'right_down', 'middle_down', 'left_up', 'middle_up', 'right_up', 'moving'], "Event can only be 'left_down', 'right_down', 'middle_down', 'left_up', 'middle_up', 'right_up', 'moving'"
-    
-    while(True):
-        ee = _getMouseEvent()
-        _ee = ee.split('_')
-        
-        if len(_ee) == 2:
-            x, y = _position()
-            if _ee[1] == 'down':
-                _mouseDown(x, y, _ee[0])
-            else:
-                _mouseUp(x, y, _ee[0])
-            
-        if ee == e:
-            return True
 
 def _keyDown(key):
     """Performs a keyboard key press without the release. This will put that
@@ -239,7 +204,7 @@ keyboardMapping.update({
     'help':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('Help')),
     'winleft':           _display.keysym_to_keycode(Xlib.XK.string_to_keysym('Super_L')),
     'winright':          _display.keysym_to_keycode(Xlib.XK.string_to_keysym('Super_R')),
-    'apps':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('Super_L')),
+    'apps':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('Menu')),
     'num0':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('KP_0')),
     'num1':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('KP_1')),
     'num2':              _display.keysym_to_keycode(Xlib.XK.string_to_keysym('KP_2')),
