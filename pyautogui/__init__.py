cc0bef23f4fb3f28400c186476ec32abb4267385
AXeL-dev <contact.axel.dev@gmail.com>
2020-04-13 12:48:21 +0100

    Customize pyautogui
    Add missing getMouseEvent() & waitForMouseEvent() functions

--- pyautogui/__init__.py
+++ pyautogui/__init__.py
@@ -1496,6 +1496,20 @@ def _mouseMoveDrag(moveOrDrag, x, y, xOffset, yOffset, duration, tween=linear, b
         failSafeCheck()
 
 
+def getMouseEvent():
+    if sys.platform.startswith('java') or sys.platform == 'darwin' or sys.platform == 'win32':
+        raise NotImplementedError('PyAutoGUI catch mouse events only for UNIX that supports X11.')
+    
+    return platformModule._getMouseEvent()
+
+
+def waitForMouseEvent(e):
+    if sys.platform.startswith('java') or sys.platform == 'darwin' or sys.platform == 'win32':
+        raise NotImplementedError('PyAutoGUI catch mouse events only supported for X11.')
+    
+    return platformModule._waitForMouseEvent(e)
+
+
 # Keyboard Functions
 # ==================
 
