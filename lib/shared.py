b4885b4533bd1354361648696e34ec00125d8831
AXeL-dev <contact.axel.dev@gmail.com>
2020-05-27 10:50:35 +0000

    Update shared.py
    Bump version

--- lib/shared.py
+++ lib/shared.py
@@ -5,7 +5,7 @@ __program_name__  = 'Dindo Bot'
 __program_desc__  = 'Farming bot for Dofus game'
 __website__       = 'https://github.com/AXeL-dev'
 __website_label__ = 'AXeL-dev'
-__version__       = '2.0.4'
+__version__       = '3.0.0-alpha'
 __authors__       = ['AXeL']
 
 class LogType:
